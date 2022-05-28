449123d41ceacd162db375e5a16025abc9701555
Michael Davis <mcarsondavis@gmail.com>
2022-05-28 14:28:30 -0500

    flesh out macros

--- grammar.js
+++ grammar.js
@@ -2,6 +2,37 @@ module.exports = grammar({
   name: "edoc",
 
   rules: {
-    source: ($) => "hello",
+    source: ($) => repeat($.line),
+
+    line: ($) => $.macro,
+
+    macro: ($) =>
+      choice(
+        $._link_macro,
+        seq("{", $.tag, optional(seq(/\s+/, $.argument)), "}")
+      ),
+    _link_macro: ($) =>
+      seq(
+        "{",
+        alias("@link", $.tag),
+        $.expression,
+        optional(seq(".", alias($.argument, $.description))),
+        "}"
+      ),
+    expression: ($) =>
+      prec.right(repeat1(choice(/[^\.}]/, $.macro_escape, "."))),
+
+    tag: ($) => /@\w+/,
+    argument: ($) => repeat1(choice(/[^}]/, $.macro_escape)),
+
+    xhtml_tag: ($) => /<.*>/,
+
+    macro_escape: ($) => choice("@{", "@}", "@@"),
+
+    double_break: ($) => /\r?\n\r?\n+/,
   },
 });
+
+function sep1(rule, separator) {
+  return seq(rule, repeat1(seq(separator, rule)));
+}
